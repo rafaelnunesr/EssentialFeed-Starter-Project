59b4c9e2a1b0c934e4a910b02192cdb5b0db274a
Rafael Rios <rafaelrafssk@hotmail.com>
2025-04-02 17:46:12 -0300

    Move FeedLoaderCacheDecorator to production

---
+++ EssentialApp/EssentialApp/FeedLoaderCacheDecorator.swift
@@ -0,0 +1,27 @@
+//
+//  FeedLoaderCacheDecorator.swift
+//  EssentialApp
+//
+//  Created by Rafael Rios on 02/04/25.
+//
+
+import EssentialFeed
+
+final class FeedLoaderCacheDecorator: FeedLoader {
+    private let decoratee: FeedLoader
+    private let cache: FeedCache
+    
+    init(decoratee: FeedLoader, cache: FeedCache) {
+        self.decoratee = decoratee
+        self.cache = cache
+    }
+    
+    func load(completion: @escaping (FeedLoader.Result) -> Void) {
+        decoratee.load { [weak self] result in
+            completion(result.map { feed in
+                self?.cache.save(feed) { _ in }
+                return feed
+            })
+        }
+    }
+}
