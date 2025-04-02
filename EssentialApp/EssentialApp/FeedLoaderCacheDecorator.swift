065c1e794f2eadcdbb28bf77eccf4a6245f715c9
Rafael Rios <rafaelrafssk@hotmail.com>
2025-04-02 18:02:51 -0300

    Remove unnecessary tests

--- EssentialApp/EssentialApp/FeedLoaderCacheDecorator.swift
+++ EssentialApp/EssentialApp/FeedLoaderCacheDecorator.swift
@@ -7,16 +7,16 @@
 
 import EssentialFeed
 
-final class FeedLoaderCacheDecorator: FeedLoader {
+public final class FeedLoaderCacheDecorator: FeedLoader {
     private let decoratee: FeedLoader
     private let cache: FeedCache
     
-    init(decoratee: FeedLoader, cache: FeedCache) {
+    public init(decoratee: FeedLoader, cache: FeedCache) {
         self.decoratee = decoratee
         self.cache = cache
     }
     
-    func load(completion: @escaping (FeedLoader.Result) -> Void) {
+    public func load(completion: @escaping (FeedLoader.Result) -> Void) {
         decoratee.load { [weak self] result in
             completion(result.map { feed in
                 self?.cache.saveIgnoringResult(feed)
