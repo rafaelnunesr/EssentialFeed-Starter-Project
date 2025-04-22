925994681aebab8087ff073e57f92e627b3ba997
Rafael Rios <rafaelrafssk@hotmail.com>
2025-04-22 20:22:43 -0300

    Record feed with error message snapshot

---
+++ EssentialFeed/EssentialFeediOS/Feed UI/Views/Helpers/UITableView+HeaderSizing.swift
@@ -0,0 +1,22 @@
+//
+//  UITableView+HeaderSizing.swift
+//  EssentialFeed
+//
+//  Created by Rafael Rios on 22/04/25.
+//
+
+import UIKit
+
+extension UITableView {
+    func sizeTableHeaderToFit() {
+        guard let header = tableHeaderView else { return }
+        
+        let size = header.systemLayoutSizeFitting(UIView.layoutFittingCompressedSize)
+        
+        let needsFrameUpdate = header.frame.height != size.height
+        if needsFrameUpdate {
+            header.frame.size.height = size.height
+            tableHeaderView = header
+        }
+    }
+}
