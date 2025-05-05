e54969c8147359607870317649e49ac2223e060b
Rafael Rios <rafaelrafssk@hotmail.com>
2025-05-05 17:37:28 -0300

    Replace FEED_VIEW_CONNECTION_ERROR key with GENERIC_CONNECTION_ERROR

--- EssentialFeed/EssentialFeedTests/Shared Presentation/LoadResourcePresenterTests.swift
+++ EssentialFeed/EssentialFeedTests/Shared Presentation/LoadResourcePresenterTests.swift
@@ -39,7 +39,7 @@ class LoadResourcePresenterTests: XCTestCase {
         
         sut.didFinishLoading(with: anyNSError())
         
-        XCTAssertEqual(view.messages, [.display(errorMessage: localized("FEED_VIEW_CONNECTION_ERROR")), .display(isLoading: false)])
+        XCTAssertEqual(view.messages, [.display(errorMessage: localized("GENERIC_CONNECTION_ERROR")), .display(isLoading: false)])
     }
     
     // MARK: - HELPERS
