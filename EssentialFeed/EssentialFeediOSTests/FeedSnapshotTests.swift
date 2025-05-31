a47e66ad5208f3901e1d640a111226a034868082
Rafael Rios <rafaelrafssk@hotmail.com>
2025-05-31 16:39:46 -0300

    Fix tests

--- EssentialFeed/EssentialFeediOSTests/FeedSnapshotTests.swift
+++ EssentialFeed/EssentialFeediOSTests/FeedSnapshotTests.swift
@@ -82,14 +82,14 @@ class FeedSnapshotTests: XCTestCase {
     }
     
     private func feedWithLoadMoreIndicator() -> [CellController] {
-        let loadMore = LoadMoreCellController()
+        let loadMore = LoadMoreCellController() {}
         loadMore.display(ResourceLoadingViewModel(isLoading: true))
         
         return feedWith(loadMore: loadMore)
     }
     
     private func feedWithLoadMoreError() -> [CellController] {
-        let loadMore = LoadMoreCellController()
+        let loadMore = LoadMoreCellController() {}
         loadMore.display(ResourceErrorViewModel(message: "This is a multiline\nerror message"))
                          
         return feedWith(loadMore: loadMore)
